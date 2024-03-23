9b97ab6b72b3d8bba62c14542321d9933bcde51e
egsch <egschaumann@gmail.com>
2024-03-23 18:25:10 -0500

    store token

--- FirstExtension/popup.js
+++ FirstExtension/popup.js
@@ -1,6 +1,14 @@
 console.log("this is a popup!")
 
 // chrome.tabs.create({url: 'index.html'});
-chrome.identity.getAuthToken({interactive: true}, function(token) {
-    console.log(token);
-  });
+chrome.storage.local.get("token", function(tokenStored){
+    if (!tokenStored) {
+        chrome.identity.getAuthToken({interactive: true}, function(token) {
+            console.log(token);
+            chrome.storage.local.set({ "token": token }, function(){});
+        });
+    } else {
+        console.log("stored token: "+ tokenStored.token);
+    }
+})
+ 
