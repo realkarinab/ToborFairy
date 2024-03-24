444df9494dc1b95783f84dded6184692e2ec3a0c
Karina Batra <karinabatra@Karinas-MacBook-Pro.local>
2024-03-24 03:28:30 -0500

    more changes

--- FirstExtension/popup.js
+++ FirstExtension/popup.js
@@ -13,7 +13,27 @@ chrome.storage.local.get("token", function(tokenStored) {
 
           const queryParams = { headers };
 
-          useToken(token);
+          fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events?' + new URLSearchParams({
+              "timeMin": new Date().toISOString()
+          }), queryParams)
+          .then((response) => response.json()) // Transform the data into json
+          .then(function(data) {
+              console.log(data);
+
+              // Process events to find free slots
+              const events = data.items || [];
+              const freeSlots = findFreeSlots(events);
+              console.log("Free Slots:", freeSlots);
+
+              // Find club events you can attend
+              const eventsYouCanAttend = clubEvents.filter(event => canAttendEvent(event, freeSlots));
+              console.log("Events you can attend:", eventsYouCanAttend);
+
+                // Save the data to Chrome storage
+            chrome.storage.local.set({ events: eventsYouCanAttend }, function() {
+                console.log("Events saved to Chrome storage");
+});
+          });
       });
   } else {
       console.log("Stored token: " + tokenStored.token);
@@ -31,12 +51,7 @@ chrome.storage.local.get("token", function(tokenStored) {
       }), queryParams)
       .then((response) => response.json()) // Transform the data into json
       .then(function(data) {
-          if (response.ok) {
-            console.log(data);
-          } else {
-            throw new Error();
-          }
-          
+          console.log(data);
 
           // Process events to find free slots
           const events = data.items || [];
@@ -47,20 +62,16 @@ chrome.storage.local.get("token", function(tokenStored) {
           const eventsYouCanAttend = clubEvents.filter(event => canAttendEvent(event, freeSlots));
           console.log("Events you can attend:", eventsYouCanAttend);
 
+        // Save the data to Chrome storage
+        chrome.storage.local.set({ events: eventsYouCanAttend }, function() {
+        console.log("Events saved to Chrome storage")});
+
           // Add events to the user's Google Calendar
           eventsYouCanAttend.forEach(event => {
             insertEventToGoogleCalendar(event, tokenStored.token);
         });
-      }).catch(function(error){
-        // if expired
-        chrome.identity.getAuthToken({ interactive: true }, function(token) {
-          console.log(token);
 
-          // Save token
-          chrome.storage.local.set({ "token": token }, function() {});;
 
-          useToken(token);
-        })
       });
   }
 });
@@ -122,34 +133,6 @@ function findFreeSlots(events) {
   return freeSlots;
 }
 
-function useToken(token) {
-  const headers = new Headers({
-    'Authorization': 'Bearer ' + token,
-    'Content-Type': 'application/json'
-});
-
-const queryParams = { headers };
-
-  fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events?' + new URLSearchParams({
-              "timeMin": new Date().toISOString()
-          }), queryParams)
-          .then((response) => response.json()) // Transform the data into json
-          .then(function(data) {
-              console.log(data);
-
-              // Process events to find free slots
-              const events = data.items || [];
-              const freeSlots = findFreeSlots(events);
-              console.log("Free Slots:", freeSlots);
-
-              // Find club events you can attend
-              const eventsYouCanAttend = clubEvents.filter(event => canAttendEvent(event, freeSlots));
-              console.log("Events you can attend:", eventsYouCanAttend);
-              return eventsYouCanAttend;
-          }).catch(function(){
-            console.log("caught!")
-          });
-}
 
 // Function to insert an event to the user's Google Calendar
 function insertEventToGoogleCalendar(event, token) {
---
+++ FirstExtension/script.js
@@ -0,0 +1,48 @@
+// // Listen for messages from the popup script (popup.js)
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//     if (message.events) {
+//         const eventsYouCanAttend = message.events;
+//         // Process eventsYouCanAttend and display them
+//         processEvents(eventsYouCanAttend);
+//     }
+// });
+
+// Function to generate event card HTML
+function createEventCard(clubName, eventName, eventTime, eventLocation) {
+    return `
+    <div class="card">
+        <div class="club-name">
+            <span class="club-text">${clubName}</span>
+        </div>
+        <div class="event-info">
+            <span class="event-name">${eventName}</span>
+            <span class="event-time">${eventTime}</span>
+            <span class="event-location">${eventLocation}</span>
+        </div>
+        <div class= "circle"></div>
+    </div>
+    `;
+}
+
+// Function to add event card to the cards container
+function addEventCard(clubName, eventName, eventTime, eventLocation) {
+    const cardsContainer = document.getElementById('cards-container');
+    const eventCardHTML = createEventCard(clubName, eventName, eventTime, eventLocation);
+    cardsContainer.innerHTML += eventCardHTML;
+}
+
+chrome.storage.local.get("events", function(data) {
+    const events = data.events;
+    console.log("Events retrieved from Chrome storage:", events);
+    // Do something with the retrieved events
+    events.forEach(event => {
+        addEventCard(event.organization, event.summary, event.start, event.link);
+    });
+});
+
+// // Function to process and display events in the DOM
+// function processEvents(eventsYouCanAttend) {
+//     eventsYouCanAttend.forEach(event => {
+//         addEventCard(event.summary, event.start.dateTime, event.organization);
+//     });
+// }
