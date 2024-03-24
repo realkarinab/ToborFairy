41311929b152c0a56d132de1b92db9346199753a
egsch <egschaumann@gmail.com>
2024-03-23 21:51:29 -0500

    finish merge

--- FirstExtension/popup.js
+++ FirstExtension/popup.js
@@ -91,3 +91,45 @@ function getFreeBusyFromToken(token) {
   });
 }
 
+// Function to find free slots between events
+function findFreeSlots(events) {
+  const sortedEvents = events.sort((a, b) => new Date(a.start.dateTime) - new Date(b.start.dateTime));
+
+  const freeSlots = [];
+  let previousEnd = new Date();
+
+  sortedEvents.forEach(event => {
+      const start = new Date(event.start.dateTime);
+      const end = new Date(event.end.dateTime);
+
+      if (start > previousEnd) {
+          freeSlots.push({ start: previousEnd, end: start });
+      }
+
+      if (event.recurrence) {
+          // Handle recurring events
+          const recurrence = event.recurrence[0]; // Assuming only one recurrence rule
+          const rule = recurrence.split(':')[1]; // Extracting the recurrence rule
+
+          // Get the start date of the event
+          let eventStart = new Date(event.start.dateTime);
+
+          // Determine the end date for recurring events (maximum 10 occurrences)
+          const maxOccurrences = 10;
+          let count = 0;
+          while (count < maxOccurrences) {
+              count++;
+              const endDate = new Date(eventStart.getTime() + 1000 * 60 * 60 * 24 * 7); // Increment by 7 days for weekly events
+              const slot = { start: eventStart, end: endDate };
+              if (endDate > previousEnd) {
+                  freeSlots.push(slot);
+              }
+              eventStart = endDate;
+          }
+      }
+
+      previousEnd = end;
+  });
+
+  return freeSlots;
+}
