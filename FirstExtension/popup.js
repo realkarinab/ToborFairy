a6b5126590b8843d1cd98c2954ed84e443d69574
egsch <egschaumann@gmail.com>
2024-03-23 20:33:56 -0500

    get events from google calendar api

--- FirstExtension/popup.js
+++ FirstExtension/popup.js
@@ -1,6 +1,20 @@
-console.log("this is a popup!")
-
 // chrome.tabs.create({url: 'index.html'});
+chrome.identity.getAuthToken({interactive: true}, function(token) {
+    console.log(token);
+
+    const headers = new Headers({
+      'Authorization' : 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    })
+
+    const queryParams = { headers };
+
+    fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
+    .then((response) => response.json()) // Transform the data into json
+    .then(function(data) {
+        console.log(data);
+    })
+  });
 chrome.storage.local.get("token", function(tokenStored){
     if (!tokenStored) {
         chrome.identity.getAuthToken({interactive: true}, function(token) {
