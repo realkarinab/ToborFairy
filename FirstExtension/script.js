fd01e6e89c68c374671301c22c71252fac7c9125
skjkanchan <skjkanchan123@gmail.com>
2024-03-24 02:34:45 -0500

    scroll

--- FirstExtension/script.js
+++ FirstExtension/script.js
@@ -1,24 +1,28 @@
 // Function to generate event card HTML
-function createEventCard(eventName, eventTime, eventLocation) {
+function createEventCard(clubName, eventName, eventTime, eventLocation) {
     return `
     <div class="card">
         <div class="club-name">
-            <span class="event-name">${eventName}</span>
+            <span class="club-text">${clubName}</span>
         </div>
         <div class="event-info">
+            <span class="event-name">${eventName}</span>
             <span class="event-time">${eventTime}</span>
             <span class="event-location">${eventLocation}</span>
         </div>
+        <div class= "circle"></div>
     </div>
     `;
 }
 
 // Function to add event card to the cards container
-function addEventCard(eventName, eventTime, eventLocation) {
+function addEventCard(clubName, eventName, eventTime, eventLocation) {
     const cardsContainer = document.getElementById('cards-container');
-    const eventCardHTML = createEventCard(eventName, eventTime, eventLocation);
+    const eventCardHTML = createEventCard(clubName, eventName, eventTime, eventLocation);
     cardsContainer.innerHTML += eventCardHTML;
 }
 
-addEventCard("Intro to React", "6-7pm", "ECSW 1.315");
-addEventCard("bye abel", "8-9pm", "abel's car");
+addEventCard("SWE","Intro to React", "6-7pm", "ECSW 1.315");
+addEventCard("Abel","bye abel", "8-9pm", "abel's car");
+addEventCard("SWE","Intro to React", "6-7pm", "ECSW 1.315");
+addEventCard("Abel","bye abel", "8-9pm", "abel's car");
