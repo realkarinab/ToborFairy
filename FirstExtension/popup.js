0135e1f8f91f4350054a5969c6792791d723025e
egsch <egschaumann@gmail.com>
2024-03-23 21:40:57 -0500

    fix authenitcation issue

--- FirstExtension/popup.js
+++ FirstExtension/popup.js
@@ -7,25 +7,14 @@ chrome.storage.local.get("token", function(tokenStored){
 
           // save token
           chrome.storage.local.set({ "token": token }, function(){});
-      
-          const headers = new Headers({
-            'Authorization' : 'Bearer ' + token,
-            'Content-Type': 'application/json'
-          })
-      
-          const queryParams = { headers };
-      
-          fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events?' + new URLSearchParams({
-            "timeMin": new Date().toISOString()
-        }), queryParams)
-          .then((response) => response.json()) // Transform the data into json
-          .then(function(data) {
-              console.log(data);
-          })
+          getEventsFromToken(token);
+          //getFreeBusyFromToken(token);
         });
 
     } else {
         console.log("stored token: "+ tokenStored.token);
+
+        // getFreeBusyFromToken(tokenStored.token);
 
         const headers = new Headers({
           'Authorization' : 'Bearer ' + tokenStored.token,
@@ -40,7 +29,57 @@ chrome.storage.local.get("token", function(tokenStored){
         .then((response) => response.json()) // Transform the data into json
         .then(function(data) {
             console.log(data);
-        })
+        }).catch(function(error) {
+            chrome.identity.getAuthToken({interactive: true}, function(token) {
+              console.log("generated new token:" + token);
+              chrome.storage.local.set({ "token": token }, function(){});
+
+              getEventsFromToken(token);
+            });
+           })
     }
 })
  
+function getEventsFromToken(token) {
+  chrome.identity.getAuthToken({interactive: true}, function(token) {
+    const headers = new Headers({
+      'Authorization' : 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    })
+
+    const queryParams = { headers };
+
+    fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events?' + new URLSearchParams({
+      "timeMin": new Date().toISOString()
+  }), queryParams)
+    .then((response) => response.json()) // Transform the data into json
+    .then(function(data) {
+        console.log(data);
+    })
+  });
+}
+
+function getFreeBusyFromToken(token) {
+  chrome.identity.getAuthToken({interactive: true}, function(token) {
+    const headers = new Headers({
+      'Authorization' : 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    })
+
+    const queryParams = { "method": "POST", body: {
+      "timeMin": new Date("2024-03-29T18:30:00-05:00"),
+      "timeMax": new Date("2024-03-29T19:30:00-05:00"),
+      "timeZone": "America/Chicago",
+      "items": [
+        {"id": "geekamunk@gmail.com"}
+      ]
+    }, headers: headers };
+
+    fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
+    .then((response) => response.json()) // Transform the data into json
+    .then(function(data) {
+        console.log(data);
+    })
+  });
+}
+
